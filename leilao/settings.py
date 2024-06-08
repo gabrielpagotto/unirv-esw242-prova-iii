1f899a4f1991c33bd4705417da187a9a52d19c58
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-06-08 17:29:05 -0300

    Add White Noise

--- leilao/settings.py
+++ leilao/settings.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.locale.LocaleMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "leilao.urls"
