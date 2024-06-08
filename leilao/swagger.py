d8405c1a6410d1d353476ad41891a1729d57332d
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-06-08 17:18:53 -0300

    Add static collected

--- leilao/swagger.py
+++ leilao/swagger.py
@@ -1,15 +1,14 @@
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="My API",
+        title="Leilão API",
+        description="Api para leilão da faculdade UniRV",
         default_version="v1",
-        description="My API description",
-        contact=openapi.Contact(email="contact@example.com"),
+        contact=openapi.Contact(email="gabriel.n.p.batista@academico.unirv.edu.br"),
         license=openapi.License(name="Awesome License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    authentication_classes=[],
 )
